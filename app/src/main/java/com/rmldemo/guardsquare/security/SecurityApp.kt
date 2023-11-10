69ad4d54e4f73328b829928bdaa61242d3592c13
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2023-11-10 14:54:26 +0700

    update new

--- app/src/main/java/com/rmldemo/guardsquare/security/SecurityApp.kt
+++ app/src/main/java/com/rmldemo/guardsquare/security/SecurityApp.kt
@@ -4,28 +4,56 @@ import android.content.Context
 import android.util.Log
 import android.widget.Toast
 
-class SecurityApp  {
+class SecurityApp {
 
 
+    companion object {
 
-    companion object{
-
-        var detection : Boolean = false
+        var detection: Boolean = false
         var pesan = ""
+        val TAG = "SecurityApp"
 
         @JvmStatic
-        fun notification(context: Context){
-            if (detection == true){
+        fun notification(context: Context) {
+            if (detection == true) {
                 Toast.makeText(context, pesan, Toast.LENGTH_SHORT).show()
                 android.os.Process.killProcess(android.os.Process.myPid())
             }
         }
 
         @JvmStatic
-        fun emulator(debugInfo: Long){
+        fun emulator(debugInfo: Long) {
             detection = true
             pesan = "Emulator Detected"
-            Log.d("SecurityApp", pesan)
+            Log.d(TAG, pesan)
         }
+
+        @JvmStatic
+        fun root(debugInfo: Long) {
+            detection = true
+            pesan = "root Detected"
+            Log.d(TAG, pesan)
+        }
+
+        @JvmStatic
+        fun hook(debugInfo: Long) {
+            detection = true
+            pesan = "hook Detected"
+            Log.d(TAG, pesan)
+        }
+        @JvmStatic
+        fun filetamper(debugInfo: Long) {
+            detection = true
+            pesan = "File Tamper Detection"
+            Log.d(TAG, pesan)
+        }
+        @JvmStatic
+        fun apptamper(debugInfo: Long) {
+            detection = true
+            pesan = "App Tamper Detection"
+            Log.d(TAG, pesan)
+        }
+
+
     }
 }
